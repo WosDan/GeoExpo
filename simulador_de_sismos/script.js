4c1342efd9857401ddae61c09c7407b080eda8f7
Daniel Rincón <daniel.rinconavarro@gmail.com>
2025-09-02 23:13:16 -0500

    Rama con un for en vez de los sismos de forma individual

--- simulador_de_sismos/script.js
+++ simulador_de_sismos/script.js
@@ -52,71 +52,31 @@ function draw(){
         [(scycle)*40, -Math.pow(scycle, 2)*20]
     ]
     
-    // for(let i = 0; i < 3; i++){
-    //     for(let j = 0; j < 2; j++){
-    //         let posorbit = Orbit(500, 500, 225, thetaOrbit1)  
-    //         let pos
-    //         if(j%2==0){
-    //             pos = Rot(funciones[i][0], funciones[i][1], theta1)  
-    //             ctx.translate(posorbit[0] + pos[0], posorbit[1] - pos[1]);      
-    //         }else{
-    //             pos = Rot(funciones[i][0], funciones[i][1], -theta1)  
-    //             ctx.translate(posorbit[0] - pos[0], posorbit[1] - pos[1]);  
-    //         }
-    //         ctx.beginPath()
-    //         ctx.arc(0, 0, 5, 0, 2*Math.PI);
-    //         ctx.fillStyle = "#727070"
-    //         ctx.fill();
-    //         ctx.stroke();  
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 2; j++){
+            let posorbit = Orbit(500, 500, 225, thetaOrbit1)  
+            let pos
+            if(j%2==0){
+                pos = Rot(funciones[i][0], funciones[i][1], theta1)  
+                ctx.translate(posorbit[0] + pos[0], posorbit[1] - pos[1]);      
+            }else{
+                pos = Rot(funciones[i][0], funciones[i][1], -theta1)  
+                ctx.translate(posorbit[0] - pos[0], posorbit[1] - pos[1]);  
+            }
+            ctx.beginPath()
+            ctx.arc(0, 0, 5, 0, 2*Math.PI);
+            ctx.fillStyle = "#727070"
+            ctx.fill();
+            ctx.stroke();  
 
-    //         if(j%2 == 0){
-    //             ctx.translate(-(posorbit[0] + pos[0]), -(posorbit[1] - pos[1]))               
-    //         }else{
-    //             ctx.translate(-(posorbit[0] - pos[0]), -(posorbit[1] -pos[1]))
-    //         } 
-    //     }
-    // }
+            if(j%2 == 0){
+                ctx.translate(-(posorbit[0] + pos[0]), -(posorbit[1] - pos[1]))               
+            }else{
+                ctx.translate(-(posorbit[0] - pos[0]), -(posorbit[1] -pos[1]))
+            } 
+        }
+    }
 
-    //Matriz de rotación para rotar la función
-    //Rot Permite rotar sobre si mismo cualquier cosa, en este caso lo utilicé para una función
-    //Rot(posición en x, posición en y, theta es el ángulo)
-    let pos = Rot(x1, y1, theta1)
-    //En este caso x y y es variable para imitar el movimiento de una función.
-    
-    theta = angle*Math.PI/180;
-
-    
-    // Parametrización de un círculo para tener una orbita
-    //Orbit es el método que hace que se mueva el punto o cualquier cosa alrededor de una orbita.
-    //Orbit(posición x de en lo que orbita, posición y de en lo que orbita, radio de la orbita, Angulo de donde está en la orbita)
-    let posorbit = Orbit(500, 500, 225, thetaOrbit1)
-
-
-    ctx.translate(posorbit[0] + pos[0], posorbit[1] - pos[1]);  
-    
-    ctx.beginPath()
-    ctx.arc(0, 0, 5, 0, 2*Math.PI);
-    ctx.fillStyle = "#727070"
-    ctx.fill();
-    ctx.stroke();
-
-    //Revertir el translate del anterior para poder crear el próximo punto
-    ctx.translate(-(posorbit[0] + pos[0]), -(posorbit[1] - pos[1]))
-    //Invertir el ángulo del rot porque en este caso como la función esta al revés en el eje x se debe rotar de manera contraria
-    pos = Rot(x1, y1, -theta1)
-    
-    ctx.translate(posorbit[0] - pos[0], posorbit[1] - pos[1]);  
-    ctx.beginPath()
-    ctx.arc(0, 0, 5, 0, 2*Math.PI);
-    ctx.fill();
-    ctx.stroke();
-
-    ctx.translate(-(posorbit[0] - pos[0]), -(posorbit[1] -pos[1]))
-     
-    ctx.beginPath()
-    ctx.arc(0, 0, 5, 0, 2*Math.PI);
-    ctx.fillStyle = "#727070"
-    ctx.fill()
     ctx.restore()
 
     window.requestAnimationFrame(draw)
