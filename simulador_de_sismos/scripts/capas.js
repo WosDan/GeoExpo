08426c1187e951a6d69655976d9a3d2d8ae8b9db
Daniel Rincón <daniel.rinconavarro@gmail.com>
2025-09-10 09:51:24 -0500

    Pink function

---
+++ simulador_de_sismos/scripts/capas.js
@@ -0,0 +1,9 @@
+class Capa{
+    constructor(){
+        this.centerX = centerX;
+        this.centerY = centerY
+        this.radius = radius
+        this.speed = speed
+        this.density = density
+    }    
+}
